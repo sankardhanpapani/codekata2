290849ae88ee77f99561212480ef12496d6028fa
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-01-07 11:16:42 +0530

    Rename oddoreven.c to oddoreven1.c

---
+++ oddoreven1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("INPUT\n");
+scanf("%d",&a);
+b=a%2;
+if(a==0)
+{
+printf("OUTPUT \nEven");
+}
+else 
+{
+printf("OUTPUT \nOdd");
+}
+return 0;
+}
