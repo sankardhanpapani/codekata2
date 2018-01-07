b31c2163d1ca250555f6fb0d34d51541ab256fc3
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-01-07 12:56:19 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("INPUT\n");
+scanf("%d",&a);
+b=a%2;
+if(b==0)
+{
+printf("OUTPUT \nEven");
+}
+else 
+{
+printf("OUTPUT \nOdd");
+}
+return 0;
+}
