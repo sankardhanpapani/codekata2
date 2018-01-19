8f6bf8bd7a30e6f9c266567578a3f462cd813aba
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-01-19 16:07:41 +0530

    Create albhabet.c

---
+++ albhabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+int n,i=0;
+char a[50];
+scanf("%s",&a[i]);
+printf("INPUT \n %s \n",a);
+n=strlen(a);
+printf("OUTPUT\n");
+for(i=0;i<=n;i++)
+{
+if(a[i]=='a' || a[i]=='b' || a[i]=='c' || a[i]=='d' || a[i]=='e' || a[i]=='f' || a[i]=='g' || a[i]=='h' || a[i]=='i' || a[i]=='j' || a[i]=='k' || a[i]=='l' || a[i]=='m' || a[i]=='n' || a[i]=='o' || a[i]=='p' || a[i]=='q' || a[i]=='r' || a[i]=='s' || a[i]=='t' || a[i]=='u' || a[i]=='v' || a[i]=='w' || a[i]=='x' || a[i]=='y' || a[i]=='z')
+printf("Albhabet");
+else
+printf("not albhabet");
+	return 0;
+}
+}
