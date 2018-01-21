8a7c9fc4c2fd539175d860652a50072243bb4855
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-01-21 12:21:46 +0530

    Create abs.c

---
+++ abs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,i;
+scanf("%d",&a);
+scanf("%d",&b);
+printf("Input \n %d %d\n",a,b);
+printf("Output\n");
+for(i=++a;i<b;i++)
+{
+for(i=2; i<=n/2; i++)
+{
+if(n%i==0)
+{
+a=1;
+break;
+}
+}
+if (a==0)
+{
+printf("Output\nYes\n");
+} 	
+
+{
+printf(" %d",i);	
+}
+}
+return 0;
+}
