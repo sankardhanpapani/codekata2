5a2db2f8c6d21de70c335ba0fdb23948391e9ac7
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-02-28 11:27:08 +0000

    Create program47.c

---
+++ program47.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() 
+{
+	int a[100],i,j,temp=0,n;
+	printf("Input\n");
+	scanf("%d",&n);
+	printf("%d\n",n);
+	for(i=1;i<=n;i++)
+	{
+	scanf("%d",&a[i]);	
+	}
+	for(i=1;i<=n;i++)
+	{
+	for(j=i+1;j<=n;j++)
+	{
+	if(a[i]>a[j])
+	{
+		temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	}
+	}
+	}
+	printf("Output\n%d %d",a[1],a[n]);
+
+	return 0;
+}
