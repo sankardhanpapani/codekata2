b292cfe0d4556c2f856adc0d57a16b59be252b72
sankardhanpapani <35135768+sankardhanpapani@users.noreply.github.com>
2018-03-14 16:33:42 +0530

    Create program68.cpp

---
+++ program68.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a,i,count=1;
+	cout<<"Input\n";
+	cin>>a;
+	cout<<a;
+	if(a%7==0)	
+	{
+		cout<<"\nOutput\nYes";
+	}
+	else
+	{
+		cout<<"\nOutput\nNo";
+	}
+	return 0;
+}
